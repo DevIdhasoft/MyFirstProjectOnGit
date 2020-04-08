a86e7b0a63ea1ec8b9aa611e2b2b8b8b3a946a03
DevIdhasoft <anuragsbhusari@gmail.com>
2020-04-08 11:07:58 -0400

    This is a important file
    chvbkdfjbkdjbdfkjbf

---
+++ EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.db.SpringJPAApplication;
+
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	
+	ArrayList<Employee> findEmployeeBySalaryBetween(int min,int max);
+	
+	
+}
